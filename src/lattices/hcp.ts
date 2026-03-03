fc36274fd26ac49179fcc05a455dc4772239a5fd
Hei Piao <heipiao233@outlook.com>
2026-03-03 21:59:47 +0800

    fix: escape

--- src/lattices/hcp.ts
+++ src/lattices/hcp.ts
@@ -3,12 +3,12 @@ import type { LatticeDefWithTags } from "../Lattice";
 
 export default {
   name: "六方堆积",
-  description: dedent`**六方堆积（HCP）** 的晶胞，底面为 60°和120° 的菱形，高为 $c = \sqrt{\frac{8}{3}} a$。
+  description: dedent`**六方堆积（HCP）** 的晶胞，底面为 60°和120° 的菱形，高为 $c = \\sqrt{\\frac{8}{3}} a$。
 
-  该晶胞包含 2 个原子，顶点上的原子为 $\frac{1}{6}$或 $\frac{1}{12}$个，中间的原子为一整个。
+  该晶胞包含 2 个原子，顶点上的原子为 $\\frac{1}{6}$或 $\\frac{1}{12}$个，中间的原子为一整个。
 
   每个原子配位数为 12：6 个位于同一密置层，3 个在上层，3 个在下层。
-  空间利用率与面心立方相同，均为 $\frac{\pi}{\sqrt{18}} \approx 74\%$。
+  空间利用率与面心立方相同，均为 $\\frac{\\pi}{\\sqrt{18}} \\approx 74\\%$。
 
   常见的具有 HCP 结构的金属有镁、钛、锌、铍、镉等。`,
   atoms: [
